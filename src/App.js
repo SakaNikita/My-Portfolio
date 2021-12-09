e6721a965e1ca261e4a4091cd270e4a76301ad69
SakaNikita <saka.pascale@gmail.com>
2021-12-09 13:22:06 +0100

    my-portfolio

--- src/App.js
+++ src/App.js
@@ -5,31 +5,9 @@ import Footer from "./Footer";
 import Home from "./Home";
 import Project from "./Project";
 import Service from "./Service";
-import {useState} from "react"
+
 
 function App() {
-
-  const [toSend, setToSend] = useState({
-    from_name: "",
-
-    to_name: "",
-
-    message: "",
-
-    reply_to: "",
-  });
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    {
-      /* --- METHOD TO SEND THE MAIL --- */
-    }
-  };
-
-  const handleChange = (e) => {
-    setToSend({ ...toSend, [e.target.name]: e.target.value });
-  };
 
   return (
     <div className="App">
--- src/Contact.js
+++ src/Contact.js
@@ -1,23 +1,99 @@
 import React from "react";
 import "./Contact.css";
-import emailjs from "emailjs"
-import {send} from "emailjs-com"
+import { useState } from "react";
 
+const Contact = () => {
 
-function Contact() {
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  function sendMail(params) {
-    var tempParams = {
-      from_name:document.getElementById("fromName").value,
-      to_name:document.getElementById("toName").value,
-      message:document.getElementById("msg").value,
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let nameS = document.getElementById("name");
+    let emailS = document.getElementById("email");
+    let messageS = document.getElementById("message");
+    let formMess = document.querySelector(".form-message");
+
+    const isEmail = () => {
+      let isMail = document.getElementById("not-mail");
+      let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+      if (email.match(regex)) {
+        isMail.style.display = "none";
+        return true;
+      } else {
+        isMail.style.display = "block";
+        isMail.style.animation = "dongle 1s";
+        setTimeout(() => {
+          isMail.style.animation = "none";
+        }, 1000);
+        return false;
+      }
     };
 
-    emailjs.send("service_zf8iycs", "template_0ftkh94", tempParams)
-    .then(function(res){
-      console.log("success", res.status);
-    })
-  }
+    if (name && isEmail() && message) {
+      const templateId = "template_0ftkh94";
+
+      nameS.classList.remove("red");
+      emailS.classList.remove("red");
+      messageS.classList.remove("red");
+
+      sendFeedback(templateId, {
+        name,
+        company,
+        phone,
+        email,
+        message,
+      });
+  } else {
+      formMess.innerHTML = "Please fill in the required fields correctly *";
+      formMess.style.background = "rgb(253, 87, 87)";
+      formMess.style.opacity = "1";
+
+      if (!name) {
+        nameS.classList.add("error");
+      }
+      if (!email) {
+        emailS.classList.add("error");
+      }
+      if (!message) {
+        messageS.classList.add("error");
+      }
+    }
+  };
+
+  const sendFeedback = (templateId, variables) => {
+    let formMess = document.querySelector(".form-message");
+
+    window.emailjs
+      .send("gmail", templateId, variables)
+      .then((res) => {
+        formMess.innerHTML =
+          "Message sent ! I will get back to you as soon as possible.";
+        formMess.style.background = "#00c1ec";
+        formMess.style.opacity = "1";
+
+        document.getElementById("name").classList.remove("error");
+        document.getElementById("email").classList.remove("error");
+        document.getElementById("message").classList.remove("error");
+        setName("");
+        setCompany("");
+        setPhone("");
+        setEmail("");
+        setMessage("");
+
+        setTimeout(() => {
+          formMess.style.opacity = "0";
+        }, 5000);
+      })
+      .catch(
+        (err) =>
+          (formMess.innerHTML = "An error has occurred. Please try again.")
+      );
+  };
 
   return (
     <div className="contact component__space" id="Contact">
@@ -37,42 +113,54 @@ function Contact() {
             <div className="input__box">
               <input
                 type="text"
-                id="toName"
-                className="contact name to_name"
+                id="name"
+                required
+                onChange={(e) => setName(e.target.value)}
+                className="contact name"
                 placeholder="Your name *"
-                value={toSend.to_name}
-                onChange={handleChange}
+                value={name}
               />
               <input
                 type="text"
                 id="fromName"
-                className="contact name from_name"
+                className="contact name"
+                onChange={(e) => setCompany(e.target.value)}
                 placeholder="Enter From Name *"
-                value={toSend.from_name}
-                onChange={handleChange}
+                value={company}
               />
-              <input
-                type="text"
-                id="msg"
-                className="contact email reply_to"
-                placeholder="Your Email *"
-                value={toSend.reply_to}
-                onChange={handleChange}
-              />
+               <input
+          type="text"
+          id="phone"
+          className="contact phone"
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="Phone"
+          value={phone}
+        />
+                <input
+                  type="text"
+                  id="email"
+                  className="contact email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Your Email *"
+                  value={email}
+                />
               <textarea
                 name="message"
                 id="message"
+                onChange={(e) => setMessage(e.target.value)}
                 placeholder="Write Your message"
-                value={toSend.message}
-                onChange={handleChange}
+                value={message}
               ></textarea>
               <button
                 className="btn contact pointer"
                 type="submit"
-                onClick={send}
+                value="send"
+                onClick={handleSubmit}
               >
                 Send Email
               </button>
+              <div className="form-message"></div>
             </div>
           </div>
         </div>
